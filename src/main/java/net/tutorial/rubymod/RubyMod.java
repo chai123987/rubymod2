a257d5b38b155178ebf08e94a82a74dc54c7a989
chai123987 <550219034@qq.com>
2026-07-21 12:46:18 +0800

    Update RubyMod.java

--- src/main/java/net/tutorial/rubymod/RubyMod.java
+++ src/main/java/net/tutorial/rubymod/RubyMod.java
@@ -1,8 +1,9 @@
 package net.tutorial.rubymod;
 
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.tutorial.rubymod.entity.ModEntities;
 
 @Mod(RubyMod.MOD_ID)
 public class RubyMod {
@@ -13,14 +14,7 @@ public class RubyMod {
 
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // 注册物品
-        // ModItems.register(bus);
-
-        // 注册方块
-        // ModBlocks.register(bus);
-
-        // 注册实体
-        // ModEntities.register(bus);
+        ModEntities.register(bus);
 
     }
 }
