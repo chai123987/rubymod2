5e90832887a6912dfdbf9eb6a2197bc5fd927955
chai123987 <550219034@qq.com>
2026-07-24 10:39:11 +0800

    Update ModBlocks.java

--- src/main/java/net/tutorial/rubymod/block/ModBlocks.java
+++ src/main/java/net/tutorial/rubymod/block/ModBlocks.java
@@ -2,6 +2,8 @@ package net.tutorial.rubymod.block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,13 +17,32 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, RubyMod.MOD_ID);
 
 
+    // 红宝石块
     public static final RegistryObject<Block> RUBY_BLOCK =
             BLOCKS.register("ruby_block",
                     () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)
                             .sound(SoundType.AMETHYST)));
 
+
+    // 红宝石矿
     public static final RegistryObject<Block> RUBY_ORE =
             BLOCKS.register("ruby_ore",
                     () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE)));
+
+
+    // 红宝石树树干
+    public static final RegistryObject<Block> RUBY_LOG =
+            BLOCKS.register("ruby_log",
+                    () -> new RotatedPillarBlock(
+                            BlockBehaviour.Properties.copy(Blocks.OAK_LOG)
+                    ));
+
+
+    // 红宝石树树叶
+    public static final RegistryObject<Block> RUBY_LEAVES =
+            BLOCKS.register("ruby_leaves",
+                    () -> new LeavesBlock(
+                            BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)
+                    ));
 
 }
