042df52ba45af68c588e546a4f57536633253e37
chai123987 <550219034@qq.com>
2026-07-24 10:49:07 +0800

    Update RubyMod.java

--- src/main/java/net/tutorial/rubymod/RubyMod.java
+++ src/main/java/net/tutorial/rubymod/RubyMod.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.tutorial.rubymod.block.ModBlocks;
 import net.tutorial.rubymod.entity.ModEntities;
+import net.tutorial.rubymod.item.ModItems;
 
 @Mod(RubyMod.MOD_ID)
 public class RubyMod {
@@ -17,6 +18,9 @@ public class RubyMod {
 
         // 注册方块
         ModBlocks.BLOCKS.register(bus);
+
+        // 注册物品
+        ModItems.register(bus);
 
         // 注册实体
         ModEntities.register(bus);
